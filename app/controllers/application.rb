1a1274dda475054143d5ddcdc70e2bc8b2da103c
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-04-27 23:47:24 +0200

    generated Rails skeleton

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'e485ce48dcda48647b6a548ea1271291'
+end
