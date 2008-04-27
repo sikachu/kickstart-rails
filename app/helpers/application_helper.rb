ab9e41120de530762a73507e4e3cf7caaf005e63
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-04-28 00:42:41 +0200

    home route; view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,41 @@
-# Methods added to this helper will be available to all templates in the application.
+begin
+  require 'rubypants'
+rescue LoadError
+  RAILS_DEFAULT_LOGGER.warn "Couldn't load RubyPants (gem install rubypants)"
+end
+
 module ApplicationHelper
+  
+  def home?
+    request.path == home_path
+  end
+
+  def active_link_to(text, path, exact = false)
+    active = ( exact ? request.path == path : (request.path+'/').index(path+'/') == 0 ) && 'current'
+    link_to text, path, :class => active
+  end
+
+  def unless_on(name)
+    path = send("#{name}_path")
+    yield(path) unless request.path == path
+  end
+  
+  # useful for stuff like dates
+  def nobreak(text)
+    text.to_s.gsub(/ +/, '&nbsp;')
+  end
+
+  # http://shauninman.com/archive/2006/08/22/widont_wordpress_plugin
+  def widont(text)
+    text.strip!
+    text[text.rindex(' '), 1] = '&nbsp;' if text.rindex(' ')
+    text
+  end
+
+  # http://daringfireball.net/projects/smartypants/
+  def smartypants(text)
+    return text unless defined? RubyPants
+    text.blank? ? '' : RubyPants.new(text).to_html
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,3 @@
-# Be sure to restart your server when you modify this file
-
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
@@ -11,13 +7,7 @@ RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  # See Rails::Configuration for more options.
-
-  # Skip frameworks you're not going to use. To use Rails without a database
-  # you must remove the Active Record framework.
+  # Skip frameworks you're not going to use.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
   # Specify gems that this application depends on. 
@@ -52,14 +42,10 @@ Rails::Initializer.run do |config|
     :secret      => '5509210e1eb9ea2238bdec5af98c074548c60ec759f8c008ae5b1af16c9bf18af2a71ef187da29775c8874557a614c11488cc394fa9bebee2c5161e16eac938d'
   }
 
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with 'rake db:sessions:create')
+  # create the session table with 'rake db:sessions:create'
   # config.action_controller.session_store = :active_record_store
 
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
+  # Use SQL instead of Active Record's schema dumper
   # config.active_record.schema_format = :sql
 
   # Activate observers that should always be running
